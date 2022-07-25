5e7b157a5d21a4cd22459a3628d402ffe47cb266
Nicolas Cabezas <nicolas.cabezas@udla.edu.ec>
2022-07-24 23:26:45 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
+
 import './App.css';
 
 function App() {
+  const url = 'https://jsonplaceholder.typicode.com/todos'
+  const [todos, setTodos] = useState()
+  const checkboxRef = useRef();
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    console.log(response.status)
+    const responseJSON = await response.json()
+    setTodos(responseJSON)
+  }
+
+  useEffect(() => {
+    fetchApi()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Only Pending</label>
+      <input type="checkbox" onChange={fetchApi}/>
+      <table stripped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>
+              Index
+            </th>
+            <th>
+              Task
+            </th>
+            <th>
+              Done
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            !todos ? 'Cargando...':
+            todos.map( (todo,index)=>{
+              return (
+                <tr>
+                  <td>{index}</td>
+                  <td>{todo.title}</td>
+                  <td>{todo.completed ? '✓' : '✕'}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
